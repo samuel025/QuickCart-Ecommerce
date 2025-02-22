902831f70bb69107b740755d67d5c74510dc6a63
Samuel <samolayemi3@gmail.com>
2025-02-22 20:42:23 +0100

    fixed usercreation event

--- config/ingest.js
+++ config/ingest.js
@@ -11,7 +11,7 @@ export const syncUserCreation = inngest.createFunction(
     {
         event: 'clerk/user.created'
     },
-    async (event) => {
+    async ({event}) => {
         const {id, first_name, last_name, email_addresses, image_url} = event.data
         const userData = {
             _id: id,
